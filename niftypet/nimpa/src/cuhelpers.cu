f694243d4acd6d3a95d82ac202daefecb87da4e8
Casper da Costa-Luis <casper.dcl@physics.org>
2021-01-16 16:40:08 +0000

    better python error handling

--- niftypet/nimpa/src/cuhelpers.cu
+++ niftypet/nimpa/src/cuhelpers.cu
@@ -1,9 +1,22 @@
+#include "Python.h"
 #include "cuhelpers.h"
-#include <cstdio>
+#include <cstdio>  // printf
+#include <sstream> // std::stringstream
 
 void HandleError(cudaError_t err, const char *file, int line) {
   if (err != cudaSuccess) {
     printf("%s in %s at line %d\n", cudaGetErrorString(err), file, line);
     exit(EXIT_FAILURE);
   }
+}
+
+bool PyHandleError(cudaError_t err, const char *file, int line) {
+  std::stringstream ss;
+  ss << file << ':' << line << ": " << cudaGetErrorString(err);
+  std::string s = ss.str();
+  if (err != cudaSuccess) {
+    PyErr_SetString(PyExc_ValueError, s.c_str());
+    return false;
+  }
+  return true;
 }
